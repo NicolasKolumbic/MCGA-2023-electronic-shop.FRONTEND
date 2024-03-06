72855374949e72db0731783dbe359db9e36a01ca
tomas arias <tomas.arias2001@gmail.com>
2024-03-06 20:24:42 -0300

    update sale and shopping cart models

--- front-end/src/models/sale.ts
+++ front-end/src/models/sale.ts
@@ -1,5 +1,13 @@
-import { Product } from "./product";
-
+import { SaleDto } from "@/dtos/sale-dto";
+import { ShoppingCart } from "./shopping-cart";
 export class Sale {
-    products!: Product[];
+    id: string;
+    detail: ShoppingCart[];
+    price: number;
+
+    constructor(saleDto?: SaleDto) {
+        this.id = saleDto?._id ?? '';
+        this.detail = saleDto?.detail.map(detail => new ShoppingCart(detail)) ?? [];
+        this.price = saleDto?.price ?? 0;
+    }
 }
--- front-end/src/models/shopping-cart.ts
+++ front-end/src/models/shopping-cart.ts
@@ -1,5 +1,14 @@
-import { Product } from "./product";
-
+import { DetailDto } from '@/dtos/detail-dto';
 export class ShoppingCart {
-    products!: Product[]
+    id: string;
+    description: string;
+    quantity: number;
+    price: number;
+
+    constructor(detailDto?: DetailDto) {
+        this.id = detailDto?._id ?? '';
+        this.description = detailDto?.description ?? '';
+        this.quantity = detailDto?.quantity ?? 0;
+        this.price = detailDto?.price ?? 0;
+    }
 }
