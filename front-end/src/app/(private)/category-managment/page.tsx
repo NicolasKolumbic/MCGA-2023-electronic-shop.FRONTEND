71810511a35263985f708b3545e15ee8c6752b51
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2024-03-12 23:50:37 -0300

    fix delete files

---
+++ front-end/src/app/(private)/category-managment/page.tsx
@@ -0,0 +1,21 @@
+import styles from "./category-managment.module.css";
+import CategoriesList from "@/components/pages/categories-list/categories-list";
+import Button from "@/components/shared/button";
+
+const CategoryManagmentPage = () => {
+
+    return <>
+        <div className="flex">
+            <div className={styles['categories__list']}>
+                <div className="flex align-middle justify-between">
+                    <h4 className={styles["categories__title"]}>Todas las Categorias</h4>
+                    <Button label={"Nueva Categoría"} design={"prussian"} link="/add-category" />
+                </div>          
+                <CategoriesList />             
+            </div>
+
+        </div>
+    </>
+};
+
+export default CategoryManagmentPage;
---
+++ front-end/src/app/(private)/product-managment/page.tsx
@@ -0,0 +1,11 @@
+import ProductManagment from "@/components/pages/product-managment";
+
+const ProductManagmentPage = () => {
+    return <>
+        <div>
+            <ProductManagment />
+        </div>
+    </>
+};
+
+export default ProductManagmentPage;
